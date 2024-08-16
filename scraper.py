930d4ee82f2878c49db93e102b9f06a87a19fd04
xboxdimensions <wlowe.at.live@gmail.com>
2024-08-16 20:24:34 +1000

    scraper + test data

--- scraper.py
+++ scraper.py
@@ -1,9 +1,17 @@
 import requests
-from bs4 import BeautifulSoup
-
+headers = requests.utils.default_headers()
+headers.update(
+    {
+        'User-Agent': 'My User Agent 1.0',
+    }
+)
 code = 2497
 url = f"https://my.uq.edu.au/programs-courses/requirements/program/{code}/2024"
 
-response = requests.get(url)
-
-print(response)
+response = requests.get(url, headers=headers)
+text = response.text
+text = str(text)
+# text = "programRequirements:" +
+text = text.split("programRequirements: ")[1]
+text = text.split(",\n routes: ")[0]
+print(text)
