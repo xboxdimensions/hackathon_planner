c6b87e424faefa1061d43f160e51201fb81c537c
Uncreeperble <be47536@gmail.com>
2024-08-17 13:33:26 +1000

    used flatten list for generate

--- FlaskApp/app.py
+++ FlaskApp/app.py
@@ -5,10 +5,17 @@ from pprint import pprint
 sys.path.insert(0, os.path.abspath('../'))
 from GenerationComponent.generate import generateOptions
 from Scaper.separated_tabs import seperatedTabs
+from Scaper.course_scraper import course_finder
 DEVELOPMENT_ENV = True
 
 app = Flask(__name__)
 
+def flatten(S):
+    if S == []:
+        return S
+    if isinstance(S[0], list):
+        return flatten(S[0]) + flatten(S[1:])
+    return S[:1] + flatten(S[1:])
 
 @app.route("/")
 def index():
@@ -22,7 +29,7 @@ def generate():
     Returns data dict containing an ordered list of all courses they could choose.
     A plan contains the prefilled required courses. 
     
-    {"AvailiableCourses" : ["COURSE1", "COURSE2", "COURSE3"],
+    {"AvailiableCourses" : [("COURSE1", "This is course 1"), ("COURSE2", "This is info 2"), ("COURSE3", "ffs")],
     "Plan": { 2024 : [["CSSE1001", "CSSE1000"],
                        ["CourseSemester2", "Course2Semester2"]],
               2025 : [["CSSE2002"], []],
@@ -33,8 +40,8 @@ def generate():
     data = request.get_json()
 
     # Get the program options & data
-    
     courses = seperatedTabs(data['ProgramCode'])
+    print(courses)
     required = courses[0]
     electives = courses[1:]
     programOptions = {"Required": required, "ProgramElectives":electives}
@@ -43,8 +50,8 @@ def generate():
     
     #
     userPlan = generateOptions(data, programOptions)
-    pprint({"AvailiableCourses":electives, "Plan":userPlan.get_return()})
-    return {"AvailiableCourses":["CSSE1001"], "Plan":userPlan.get_return()}
+    pprint({"AvailiableCourses":flatten(electives), "Plan":userPlan.get_return()})
+    return {"AvailiableCourses":flatten(electives), "Plan":userPlan.get_return()}
      
 
 if __name__ == "__main__":
